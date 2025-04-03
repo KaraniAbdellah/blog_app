07b78a57f17a2517a195f4ac936d62887a8b0743
KaraniAbdellah <abdellahkarani@gmail.com>
2025-04-03 19:26:07 +0000

    fix editor

--- client/src/pages/createBlog.jsx
+++ client/src/pages/createBlog.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-import { useState, useRef} from "react";
+import { useState, useRef } from "react";
 import JoditEditor from "jodit-react";
 import HTMLReactParser from "html-react-parser";
 
 const CreateBlog = () => {
   const editor = useRef(null);
-  const [content, setContent] = useState(null);
+  const [content, setContent] = useState("");
 
   return (
-    <div className="p-3 bg-white">
-      <form>
+    <div className="p-3 bg-white w-full px-8 flex justify-center items-center">
+      <div className="md:w-[80%] w-full">
+        <form className="flex flex-col my-4 w-full">
+          <input
+            required
+            className="border w-full password rounded-sm bg-gray-50 outline-none border-non p-2 mb-2"
+            type="text"
+            placeholder="Title..."
+          />
+          <textarea
+            required
+            className="border w-full password rounded-sm bg-gray-50 outline-none border-non p-2 mb-2"
+            type="text"
+            placeholder="Short Description..."
+          ></textarea>
+          <input
+            required
+            className="border w-full password rounded-sm bg-gray-50 outline-none border-non p-2 mb-2"
+            type="file"
+            placeholder="Title..."
+          />
+        </form>
+        <div className="editor w-full">
+          <JoditEditor
+            ref={editor}
+            value={content}
+            onChange={(newContent) => setContent(newContent)}
+          ></JoditEditor>
 
-      </form>
-      <JoditEditor
-        ref={editor}
-        value={content}
-        onChange={(newContent) => setContent(newContent)}
-      ></JoditEditor>
+          <div className="content p-3 bg-gray-100 border my-5">
+            {HTMLReactParser(content)}
+          </div>
 
-      <div className="content">{HTMLReactParser(content)}</div>
-      <button className="text-end bg-green-700 text-white rounded-sm px-2">Publish</button>
+          <button className="bg-sky-700 hover:bg-sky-800 font-medium text-white rounded-sm px-8 py-2">
+            Publish
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
